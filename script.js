8f9352520b797f3e8cbc3d2280f13e7322a50ed4
kullop <burakcankarasel@gmail.com>
2022-06-12 13:53:25 +0300

    Made changes on style.css changed appearance, index.html wrapped ul with a div, script.js added local storage

--- script.js
+++ script.js
@@ -1,35 +1,115 @@
-const input = document.querySelector("input")
+const input = document.querySelector("input");
+const addBtn = document.querySelector("#add");
+const list = document.querySelector("ul");
 
-const addBtn = document.querySelector("#add")
+let liItems = JSON.parse(localStorage.getItem("todos")) || [];
 
-const list = document.querySelector("ul")
+window.onload = function(){
+    input.focus();
+};
 
-const doneBtns = document.querySelectorAll(".done")
+renderSavedTodos();
 
+function renderSavedTodos(){
+    liItems.forEach(li => {
+        createListElement(li);
+    });
+};
 
-const delEvent = function(){
-    this.parentElement.remove()
+function createListElement(item) {
+    const {id, content, isDone} = item;
+    list.innerHTML += `
+        <li id="${id}" class="${isDone}">
+        <button class="done">
+        <ion-icon name="checkmark-outline" class="done-i">
+        </ion-icon>
+        </button>
+        ${content}
+        <button class="del">
+        <ion-icon name="close-outline" class="del-i">
+        </ion-icon>
+        </button>
+        </li>
+        `;
 }
 
-const doneEvent = function(){
-    this.parentElement.classList.toggle("done-event")
-}
-
+// addBtn function
 addBtn.addEventListener("click", () => {
     if(input.value.trim()){
-        list.innerHTML += `
-        <li><button class="done"><ion-icon name="checkmark-outline"></ion-icon></button>${input.value}<button class="del"><ion-icon name="close-outline"></ion-icon></button></li>
-        `
-        input.value = ''
+        const liItem = {
+            id: new Date().getTime(),
+            isDone: false,
+            content: input.value
+        };
+
+        liItems.push(liItem)
+
+        localStorage.setItem("todos", JSON.stringify(liItems))
+
+        createListElement(liItem);
+        input.value = ""
     }
+})
 
-    const delBtns = document.querySelectorAll(".del")
+// adding todo with enter button
+input.addEventListener("keydown", (e) => {
+    if(e.key === "Enter"){
+       addBtn.click()
+    }
+})
 
-    delBtns.forEach(btn => btn.addEventListener("click", delEvent))
+//arranging done & del buttons functions
+list.addEventListener("click", (e) => {
+    if(e.target.classList.contains("done") || e.target.classList.contains("del")){
+        const id = e.target.parentElement.getAttribute("id")
 
+        if(e.target.classList.contains("done")){
+            liItems.map((item, index, arr) => {
+                if(item.id == id) {
+                    arr[index].isDone = !arr[index].isDone
+                }
+            });
 
-    const doneBtns = document.querySelectorAll(".done")
+            localStorage.setItem("todos", JSON.stringify(liItems))
+        e.target.parentElement.classList.contains("done-event")
+        ?
+        e.target.parentElement.classList.remove("done-event")
+        :
+        e.target.parentElement.classList.add("done-event")
+        
+        }
+        if(e.target.classList.contains("del")){
 
-    doneBtns.forEach(btn => btn.addEventListener("click", doneEvent))
+            liItems = liItems.filter(item => item.id != id)
 
+            localStorage.setItem("todos", JSON.stringify(liItems))
+
+            e.target.parentElement.remove()
+        }
+    }
+    if(e.target.classList.contains("done-i") || e.target.classList.contains("del-i")){
+        const id = e.target.parentElement.parentElement.getAttribute("id")
+        if(e.target.classList.contains("done-i")){
+            liItems.map((item, index, arr) => {
+                if(item.id == id) {
+                    arr[index].isDone = !arr[index].isDone
+                }
+            });
+
+            e.target.parentElement.parentElement.classList.contains("done-event")
+            ?
+            e.target.parentElement.parentElement.classList.remove("done-event")
+            :
+            e.target.parentElement.parentElement.classList.add("done-event")
+
+            localStorage.setItem("todos", JSON.stringify(liItems))
+        }
+        if(e.target.classList.contains("del-i")){
+            liItems = liItems.filter(item => item.id != id)
+
+            localStorage.setItem("todos", JSON.stringify(liItems))
+
+            e.target.parentElement.parentElement.remove()
+        }
+    }
 })
